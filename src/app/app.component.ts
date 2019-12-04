5416cf9d28cf1516ea0dfa9f187d1142aa457fb5
mikeve97 <mikevanes97@gmail.com>
2019-12-04 22:16:34 +0100

    gmaps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {GmapsService} from './gmaps/gmaps.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [GmapsService]
 })
 export class AppComponent {
   title = 'DF';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GmapsComponent } from './gmaps/gmaps.component';
+import { GmapsLocSearchComponent } from './gmaps/gmaps-loc-search/gmaps-loc-search.component';
+import { MenuComponent } from './menu/menu.component';
+import {FormsModule} from '@angular/forms';
+import {GooglePlaceModule} from 'ngx-google-places-autocomplete';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GmapsComponent,
+    GmapsLocSearchComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    GooglePlaceModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
