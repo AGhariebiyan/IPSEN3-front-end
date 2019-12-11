17538e3e4233a8b600ccc36f3f68270a3b8f71ca
ofahchouch <ofahchouch@yahoo.com>
2019-12-11 13:17:29 +0100

    testing..

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -13,6 +13,13 @@ export class DashboardComponent implements OnInit {
   }
 
   onClickPost() {
+    const fetchedObj = this.httpClientService.onGet('http://localhost:8080/trips/trip/98');
+
+    for(let key in fetchedObj) {
+      console.log(fetchedObj[key].trip);
+    }
+
+    // console.log(fetchedObj);
   }
 
 }
