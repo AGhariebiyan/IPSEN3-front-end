26304cd23f98bce1966ea3c9e2af3409799efa39
aghariebiyan <alirezaa_afg@live.nl>
2019-12-13 17:40:18 +0100

    trips toevoegen werkt

--- src/app/trips/trip-add/trip-add.component.ts
+++ src/app/trips/trip-add/trip-add.component.ts
@@ -1,32 +1,50 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { HttpClientService } from 'src/app/shared/http-client.service';
+import { GmapsService } from 'src/app/gmaps/gmaps.service';
 
 @Component({
   selector: 'app-trip-add',
   templateUrl: './trip-add.component.html',
-  styleUrls: ['./trip-add.component.css']
+  styleUrls: ['./trip-add.component.css'],
+  providers: [GmapsService]
 })
 export class TripAddComponent implements OnInit {
-
+  formSubmitted = false;
   tripAddForm: FormGroup;
 
-  constructor() { }
+  public drivenKilometers;
+  public estTravelTime;
+
+  constructor(private httpClientServive: HttpClientService, private cdr: ChangeDetectorRef, private mapService: GmapsService) { }
 
   ngOnInit() {
     this.tripAddForm =  new FormGroup({
-      'licenseplate': new FormControl(null, Validators.required),
-      'startLocation': new FormControl(null, Validators.required),
-      'endLocation': new FormControl(null, Validators.required),
-      'drivenKm': new FormControl(null, Validators.required),
+      'licenseplate': new FormControl(null),
+      'startLocation': new FormControl(null),
+      'endLocation': new FormControl(null),
+      'drivenKm': new FormControl(null),
+      'startKmGauge': new FormControl(null),
+      'endKmGauge': new FormControl(null),
+      'projectID': new FormControl(null)
     });
+    
+    this.mapService.drivenKilometers.subscribe((km) => {this.drivenKilometers = km; this.cdr.detectChanges(); } );
+    this.mapService.estTravelTime.subscribe((time) => {this.estTravelTime = time; this.cdr.detectChanges(); } );
   }
 
   onSubmit(){
-    console.log(this.tripAddForm);
-    console.log(this.tripAddForm.value.licenseplate);
-    console.log(this.tripAddForm.value.startLocation);
-    console.log(this.tripAddForm.value.endLocation);
-    console.log(this.tripAddForm.value.drivenKm);
+    const licenseplate = this.tripAddForm.value.licenseplate;
+    const startLocation = this.tripAddForm.value.startLocation;
+    const endLocation = this.tripAddForm.value.endLocation;
+    const drivenKm = this.tripAddForm.value.drivenKm;
+    const startKmGauge = this.tripAddForm.value.startKmGauge;
+    const endKmGauge = this.tripAddForm.value.endKmGauge;
+    const projectId = this.tripAddForm.value.projectID;
+
+    const postObj = this.httpClientServive.onPost(
+      'http://localhost:8080/trips/trip/add/for-project/' + projectId + '/1/' + licenseplate + '/' + startLocation + '/' + endLocation + '/' + startKmGauge + '/' + endKmGauge + '/' + drivenKm);
+
   }
 
 }
