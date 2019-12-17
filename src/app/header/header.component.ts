327318e491f49bdb8806fb17cc4edc2a2b2a0fa9
wnicolaas <wietsenicolaas@gmail.com>
2019-12-17 15:41:12 +0100

    Rewriting header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,21 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  overlayNavMenuValue: string = 'none';
+  events: string[] = [];
+  opened: boolean;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  onOverlay() {
-    if (this.overlayNavMenuValue == 'block') {
-      this.overlayNavMenuValue = 'none';
-      (document.querySelector('#overlay') as HTMLElement).style.display = this.overlayNavMenuValue;
-    } else {
-      this.overlayNavMenuValue = 'block';
-      (document.querySelector('#overlay') as HTMLElement).style.display = this.overlayNavMenuValue;
-      (document.querySelector('body') as HTMLElement).style.overflow = 'hidden';
-    }
-  }
+
 }
