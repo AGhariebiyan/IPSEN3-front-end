ed620c77e83ace74428512c4340a220a64ad6f15
aghariebiyan <alirezaa_afg@live.nl>
2019-12-19 10:45:51 +0100

    routing toegevoegd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,13 +10,21 @@ import { NotificationComponent } from './shared/notification/notification.compon
 import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 
 import { ProjectOverviewPageComponent } from './project/project-overview-page/project-overview-page.component';
+import { VehicleAddComponent } from './vehicles/vehicle-add/vehicle-add.component';
+import { TripAddComponent } from './trips/trip-add/trip-add.component';
 
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
     { path: 'dashboard', component: DashboardComponent},
-    { path: 'ritten', component: TripsComponent},
-    { path: 'voertuigen', component: VehiclesComponent},
+    { path: 'ritten', component: TripsComponent,
+    children: [
+        { path: 'toevoegen', component: TripAddComponent },
+      ]},
+    { path: 'voertuigen', component: VehiclesComponent, 
+    children: [
+        { path: 'toevoegen', component: VehicleAddComponent},
+      ]},
     { path: 'ritten/inzien', component: ViewTripsPageComponent},
     { path: 'projecten', component: ProjectOverviewPageComponent},
     { path: 'projecten/:projectId', component: ProjectInzienPageComponent},
