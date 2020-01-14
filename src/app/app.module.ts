162a378320511c87d4fbab23c6dbf7211d39b5c5
wnicolaas <wietsenicolaas@gmail.com>
2020-01-14 15:20:11 +0100

    Fixed some styling issues that came with the new header, updated routerlinks and module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ViewTripsPageComponent } from './view-trips-page/view-trips-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './header/header.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ProjectInzienPageComponent } from './project/project-inzien-page/project-inzien-page.component';
