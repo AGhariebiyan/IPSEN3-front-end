68c749156fe4385f741e57d5e6f4c3a952d481ac
ofahchouch <ofahchouch@yahoo.com>
2020-01-14 21:46:54 +0100

    added: header and body to the request

--- src/app/shared/http-client.service.ts
+++ src/app/shared/http-client.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {map, catchError} from 'rxjs/operators';
 import {ArrayType} from '@angular/compiler';
 import {Observable} from 'rxjs';
@@ -9,6 +9,25 @@ import {Observable} from 'rxjs';
 })
 export class HttpClientService {
   constructor(private http: HttpClient) {
+    let httpHeaders = new HttpHeaders({
+      'Content-Type' : 'application/json',
+      'Token' : 'Hier komt een JWToken'
+    }); 
+
+    let options = {
+      headers: httpHeaders
+    };  
+
+    const postData = {
+      username: 'admin',
+      password: 'root'
+    };
+
+    let fruits: Array<string> = ['Apple', 'Orange', 'Banana'];
+    
+    this.http.post<any>("http://localhost:8080/trips/testingPOST", JSON.stringify(fruits), options).toPromise().then((data:any) => {
+      console.log(data);
+    })
   }
 
   onGet(getUrl: string): Observable<any> {
