6b1130dfdb8512d463227b929548481dd601d389
wnicolaas <wietsenicolaas@gmail.com>
2020-01-23 13:20:37 +0100

    Removed console logs and fixed TSlinting issues (except for line length but will be fixed later)

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,8 +7,8 @@ import { HttpClientService } from '../shared/http-client.service';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  amountOfTripsByUser = "loading..";
-  amountOfProjectsByUser = "loading..";
+  amountOfTripsByUser = 'loading..';
+  amountOfProjectsByUser = 'loading..';
 
   constructor(private httpClientService: HttpClientService) {}
 
@@ -17,11 +17,9 @@ export class DashboardComponent implements OnInit {
   }
 
   fetchAmountOfTripsAndProjectsByUser() {
-    const fetchedObj = this.httpClientService.onGet('http://localhost:8080/trips/fetch/unique-projectids-and-trips-amount/' + localStorage.getItem('userid')).pipe()
+    this.httpClientService.onGet('http://localhost:8080/trips/fetch/unique-projectids-and-trips-amount/' + localStorage.getItem('userid')).pipe()
     .subscribe(
       data => {
-        console.log('data 0 = ' + data[0]);
-        console.log('data 1 = ' + data[1]);
         this.amountOfTripsByUser = data[0];
         this.amountOfProjectsByUser = data[1];
       },
