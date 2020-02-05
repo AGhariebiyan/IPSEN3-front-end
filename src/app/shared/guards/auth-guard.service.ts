bff89bb2b9bc53588f13b09a626a8365a89ffbb4
ofahchouch <ofahchouch@yahoo.com>
2020-02-05 17:02:33 +0100

    saving changes, hopping to other branch

--- src/app/shared/guards/auth-guard.service.ts
+++ src/app/shared/guards/auth-guard.service.ts
@@ -1,22 +1,43 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from 'src/app/auth/auth.service';
+import { HttpClientService } from '../http-client.service';
+import { DOCUMENT } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuardService implements CanActivate {
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router, private httpClientService: HttpClientService, @Inject(DOCUMENT) private document: Document) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return this.authService.isLoggedIn().then((loggedIn: boolean) => {
+    return this.guard().then((loggedIn: boolean) => {
       if (loggedIn) {
         return true;
       } else {
-        this.router.navigate(['/']);
+        // this.router.navigate(['https://www.google.nl/']);
+        // this.document.location.href = 'https://www.google.nl/';
       }
     });
+  }
+
+  guard(): Promise<boolean> {
+    return new Promise(
+      (resolve, reject) => {
+        this.httpClientService.onPost('/auth', null).subscribe(
+          data => {
+            if (data) {
+              resolve(true);
+            } else {
+              resolve(false);
+            }
+          },
+          error => {
+            console.log(error);
+          });
+      }
+    );
   }
 }
