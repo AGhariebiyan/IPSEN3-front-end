8b2d5053103143d3836fe3475ba1d4d624d23a42
ofahchouch <ofahchouch@yahoo.com>
2020-02-05 20:04:09 +0100

    fixed: the guard for the routes

--- src/app/shared/guards/auth-guard.service.ts
+++ src/app/shared/guards/auth-guard.service.ts
@@ -1,43 +1,52 @@
 import { Injectable, Inject } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject, empty } from 'rxjs';
 import { AuthService } from 'src/app/auth/auth.service';
 import { HttpClientService } from '../http-client.service';
-import { DOCUMENT } from '@angular/common';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuardService implements CanActivate {
+  private httpHeaders = new HttpHeaders({
+    'Content-Type' : 'application/json'
+  });
+  private urlStart = 'http://localhost:8080';
 
-  constructor(private authService: AuthService, private router: Router, private httpClientService: HttpClientService, @Inject(DOCUMENT) private document: Document) {}
+  constructor(private authService: AuthService, private router: Router, private http: HttpClient) {  }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
     return this.guard().then((loggedIn: boolean) => {
       if (loggedIn) {
         return true;
       } else {
-        // this.router.navigate(['https://www.google.nl/']);
-        // this.document.location.href = 'https://www.google.nl/';
+        localStorage.clear();
+        this.router.navigate(['/login']);
       }
     });
   }
 
-  guard(): Promise<boolean> {
-    return new Promise(
-      (resolve, reject) => {
-        this.httpClientService.onPost('/auth', null).subscribe(
-          data => {
-            if (data) {
-              resolve(true);
-            } else {
-              resolve(false);
-            }
-          },
-          error => {
-            console.log(error);
-          });
-      }
-    );
+  private guard(): Promise<boolean> {
+      return new Promise(
+        (resolve, reject) => {
+          if (localStorage.getItem('jwtoken') !== null && this.httpHeaders.get('Token') === null) {
+            this.httpHeaders = this.httpHeaders.append('Token', localStorage.getItem('jwtoken'));
+          }
+
+          this.http.post('http://localhost:8080/auth', null, {headers: this.httpHeaders}).subscribe(
+            data => {
+              console.log("data: " + data);
+              if(data) {
+                resolve(true);
+              } else {
+                resolve(false);
+              }
+            },
+            error => {
+              console.log(error);
+            });
+        }
+      );
   }
 }
